e253dd8c6fc12305e2c23e771b201aaebaf916c7
chris <chris@chriss-iMac.local>
2021-03-13 16:07:45 +0800

    Initial commit

---
+++ whatever2.js
@@ -0,0 +1,187 @@
+var colors = ["red", "green", "blue", "cyan", "magenta"];
+
+Plotly.d3.csv("/data_csv_plotly.csv", function (err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function (row) {
+      return row[key];
+    });
+  }
+
+  var trace1 = {
+    x: unpack(rows, "x1"),
+    y: unpack(rows, "y1"),
+    z: unpack(rows, "z1"),
+    mode: "markers",
+    marker: {
+      size: 2,
+      line: {
+        color: "red",
+        width: 0.5,
+      },
+      opacity: 0.8,
+    },
+    type: "scatter3d",
+  };
+
+  var trace2 = {
+    x: unpack(rows, "x2"),
+    y: unpack(rows, "y2"),
+    z: unpack(rows, "z2"),
+    mode: "markers",
+    marker: {
+      color: "green",
+      size: 2,
+      symbol: "circle",
+      line: {
+        color: "rgb(204, 204, 204)",
+        width: 1,
+      },
+      opacity: 0.8,
+    },
+    type: "scatter3d",
+  };
+
+  var trace2 = {
+    x: unpack(rows, "x3"),
+    y: unpack(rows, "y3"),
+    z: unpack(rows, "z3"),
+    mode: "markers",
+    marker: {
+      color: "blue",
+      size: 2,
+      symbol: "circle",
+      line: {
+        color: "rgb(204, 204, 204)",
+        width: 1,
+      },
+      opacity: 0.8,
+    },
+    type: "scatter3d",
+  };
+
+  var trace3 = {
+    x: unpack(rows, "x4"),
+    y: unpack(rows, "y4"),
+    z: unpack(rows, "z4"),
+    mode: "markers",
+    marker: {
+      color: "green",
+      size: 2,
+      symbol: "circle",
+      line: {
+        color: "rgb(204, 204, 204)",
+        width: 1,
+      },
+      opacity: 0.8,
+    },
+    type: "scatter3d",
+  };
+
+  var trace4 = {
+    x: unpack(rows, "x5"),
+    y: unpack(rows, "y5"),
+    z: unpack(rows, "z5"),
+    mode: "markers",
+    marker: {
+      color: "cyan",
+      size: 2,
+      symbol: "circle",
+      line: {
+        color: "rgb(204, 204, 204)",
+        width: 1,
+      },
+      opacity: 0.8,
+    },
+    type: "scatter3d",
+  };
+  
+  var trace5 = {
+    x: unpack(rows, "x2"),
+    y: unpack(rows, "y2"),
+    z: unpack(rows, "z2"),
+    mode: "markers",
+    marker: {
+      color: "magenta",
+      size: 2,
+      symbol: "circle",
+      line: {
+        color: "rgb(204, 204, 204)",
+        width: 1,
+      },
+      opacity: 0.8,
+    },
+    type: "scatter3d",
+  };
+
+  var data = [trace1, trace2, trace3, trace4, trace5];
+  var layout2 = {
+    
+        title: 'Sales Growth',
+        scene:{
+            
+          xaxis: {
+           type: "log",
+           range: [0, 2.3],
+         },
+          yaxis: {
+           
+           range: [0, 0.6],
+         },
+          zaxis: {
+          
+          range: [-60, -10],
+         }},
+    //     yaxis: {
+    //       title: 'Percent',
+    //       showline: false
+    //     },
+
+      
+    margin: {
+      l: 0,
+      r: 0,
+      b: 0,
+      t: 0,
+    },
+
+    width: '1000',
+    height: '1000'
+  };
+
+  var layout = {
+    
+    title: 'Sales Growth',
+    scene:{
+        
+      xaxis: {
+       type: "log",
+       range: [0, 4],
+     },
+      yaxis: {
+       
+       range: [0, 0.6],
+     },
+      zaxis: {
+      
+      range: [-60, -10],
+     }},
+//     yaxis: {
+//       title: 'Percent',
+//       showline: false
+//     },
+
+  
+margin: {
+  l: 0,
+  r: 0,
+  b: 0,
+  t: 0,
+},
+
+width: '1000',
+height: '1000'
+};
+
+  Plotly.newPlot("myDiv", data, layout);
+  Plotly.newPlot("myDivLower", data, layout2);
+});
